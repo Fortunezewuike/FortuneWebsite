5ae2ac1c2bf3bcca19fc8749826d9734a1792c06
Fortunezewuike <119663393+Fortunezewuike@users.noreply.github.com>
2026-04-07 08:40:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
+import Card from "./Components/Card";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <Navbar />
       <Hero />
 
       <Card title="Ezewuike" description="This is my Surn name" />
       <Card title="Fortune Uzoma" description="This are my other names 2" />
       <Card title="Run/cyb/25/18840" description="This is my matric number " />
+      <Footer />
     </div>
   );
 }
